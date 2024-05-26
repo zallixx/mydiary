88c846537c839ab634272f6fa61305e051f1895f
Алексей Морозов <almoroz08@gmail.com>
2024-05-26 23:21:25 +0300

    added relation Profile-School, edited setup page(also added png to setup page)

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,7 +1,35 @@
-import InitializeProfile from "@/lib/initualize-profile";
+import InitializeProfile from '@/lib/initualize-profile';
+import db from '@/lib/db';
+import { redirect } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default async function SetupPage() {
     const profile = await InitializeProfile();
 
-    return <div>123</div>;
+    if(profile.schoolId !== null) {
+        const school = await db().school.findUnique({
+            where: {
+                id: profile.schoolId
+            }
+        });
+
+        if (school) {
+            redirect('/diary');
+        }
+    }
+
+    return (
+        <div className="flex justify-items-center items-center justify-center h-screen flex-col select-none">
+            <Image src={'/fault-in-the-search.png'} alt={'Ошибка при поиске учебного заведения'} className="pointer-events-none" width={250} height={250} />
+            <p className="text-black max-w-2xl text-center">
+                В настоящее время информация о вашем зачислении в школу отсутствует.
+                Возможно, стоит немного подождать, пока данные будут обновлены в системе.
+                Если же по прошествии времени ситуация не изменится, рекомендуем вам
+                обратиться в службу поддержки школы для получения более подробной
+                информации и помощи или{' '}
+                <Link href={'/support'} className="text-blue-500">написать в поддержку сайта дневника.</Link>
+            </p>
+        </div>
+    );
 }
