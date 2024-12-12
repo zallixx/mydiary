7383f6c25d69a870b2b8c16da5cf3aa19587a357
Морозов Алексей <almoroz08@gmail.com>
2024-12-13 01:59:41 +0300

    исправил недочёты

--- components/diary/schedule/lesson-sheet.tsx
+++ components/diary/schedule/lesson-sheet.tsx
@@ -6,10 +6,11 @@ import {
     SheetTitle,
 } from '@/components/ui/sheet';
 import { itemProps } from '@/components/diary/schedule/lesson-item';
-import { validateDate, getLessonTime } from '@/components/diary/schedule/functions';
+import {validateDate, getLessonTime, setPropForItem, setIndexForGrade} from '@/components/diary/schedule/functions';
 import { ReactElement } from 'react';
 import { Checkbox } from "@/components/ui/checkbox"
 import { ScrollArea } from '@/components/ui/scroll-area';
+import * as React from "react";
 
 interface homeworkProps {
     id: string;
@@ -31,30 +32,32 @@ interface specificAssignmentsProps {
     }[];
 }
 
+interface assessmentProps {
+    comment: string;
+    category: string;
+    gradeType: string;
+    grade: number;
+}
+
 export default function LessonSheet({ open, onOpenChange, item, date }: { open: boolean; onOpenChange: () => void; item: itemProps; date: Date }) {
-    const lessonComponents: { title: string; children: { sub_title?: string; svg?: ReactElement; description?: string; homeworkList?: homeworkProps[]; specificAssignments?: specificAssignmentsProps[]; }[] }[] = [
+    const lessonComponents: { title: string; children: { sub_title?: string; svg?: ReactElement; description?: string; homeworkList?: homeworkProps[]; specificAssignments?: specificAssignmentsProps[]; assessment?: assessmentProps[] }[] }[] = [
         {
-            title: item.event_type === 'LESSON' ? "Об уроке:" : "О мероприятии:",
+            title: item.event_type === 'LESSON' ? "Об уроке" : "О мероприятии",
             children: [
                 {
                     sub_title: "Тип мероприятия",
                     svg: <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-info mr-1"><circle cx="12" cy="12" r="10"/><path d="M12 16v-4"/><path d="M12 8h.01"/></svg>,
-                    description: item.event_type,
-                },
-                {
-                    sub_title: "Дата и время проведения",
-                    svg: <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-clock mr-1"><circle cx="12" cy="12" r="10"/><polyline points="12 6 12 12 16 14"/></svg>,
-                    description: date.toISOString().split('T')[0] + ' ' + new Date(item.baseSchedule.date).getUTCHours().toString().padStart(2, '0') + ':' + new Date(item.baseSchedule.date).getUTCMinutes().toString().padStart(2, '0') + ' - ' + new Date(new Date(item.baseSchedule.date).getTime() + item.baseSchedule.duration * 60000).getUTCHours().toString().padStart(2, '0') + ':' + new Date(new Date(item.baseSchedule.date).getTime() + item.baseSchedule.duration * 60000).getUTCMinutes().toString().padStart(2, '0'),
+                    description: item.event_type + '',
                 },
                 {
                     sub_title: "Место проведения",
                     svg: <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-map-pin mr-1"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><circle cx="12" cy="10" r="3"/></svg>,
-                    description: item.place,
+                    description: item.place + '',
                 },
             ],
         },
         {
-            title: "Преподаватель:",
+            title: "Преподаватель",
             children: [
                 {
                     description: item.baseSchedule.teacher.name,
@@ -62,7 +65,7 @@ export default function LessonSheet({ open, onOpenChange, item, date }: { open: 
             ],
         },
         {
-            title: "Домашние задания:",
+            title: "Домашние задания",
             children: [
                 {
                     homeworkList: item.homework,
@@ -70,10 +73,18 @@ export default function LessonSheet({ open, onOpenChange, item, date }: { open: 
             ]
         },
         {
-            title: "Личные задания:",
+            title: "Личные задания",
             children: [
                 {
                     specificAssignments: item.specificAssignment,
+                },
+            ]
+        },
+        {
+            title: "Результаты урока",
+            children: [
+                {
+                    assessment: item.assessment,
                 },
             ]
         },
@@ -100,16 +111,21 @@ export default function LessonSheet({ open, onOpenChange, item, date }: { open: 
                                 if (component.title === "Личные задания:" && (!component.children[0].specificAssignments || component.children[0].specificAssignments.length === 0)) {
                                     return null;
                                 }
+                                if (component.title === "Результаты урока" && (!component.children[0].assessment || component.children[0].assessment.length === 0)) {
+                                    return null;
+                                }
                                 return (
                                     <span className="p-[24px] rounded-[16px] bg-white items-start justify-items-start content-start flex flex-col text-[#87879b]" key={index}>
                                         <span className="font-semibold text-lg text-black">{component.title}</span>
                                         {component.children.map((child, childIndex) => (
-                                            <span key={childIndex} className={`text-base w-full ${component.title === 'Преподаватель:' || component.title === 'Домашние задания:' || component.title === 'Личные задания:' ? '' : 'pl-1'} flex flex-col`}>
+                                            <span key={childIndex}
+                                                  className={`text-base w-full ${component.title === 'Преподаватель' || component.title === 'Домашние задания' || component.title === 'Личные задания' || component.title === 'Результаты урока'  ? '' : 'pl-1'} flex flex-col`}>
                                                 <span className="flex flex-row items-center">
                                                     {child.svg}
                                                     {child.sub_title}
                                                 </span>
-                                                <span className={`flex flex-row items-center text-black ${component.title === 'Преподаватель:' ? '' : 'pl-6'}`}>
+                                                <span
+                                                    className={`flex flex-row items-center text-black ${component.title === 'Преподаватель' ? '' : 'pl-6'}`}>
                                                     {child.description}
                                                 </span>
                                                 {child.homeworkList && child.homeworkList.map((homework, homeworkIndex) => (
@@ -117,8 +133,11 @@ export default function LessonSheet({ open, onOpenChange, item, date }: { open: 
                                                         <span className="flex flex-row items-center text-black">
                                                             {homework.description}
                                                         </span>
-                                                        <span className={`w-full mt-[16px] mb-2 h-[48px] text-black p-[16px] rounded-[16px] flex items-center cursor-pointer ${homework.completions[0]?.isCompleted ? 'bg-[#e8f7ea]' : 'bg-[#f4f4f8]'}`}>
-                                                            <Checkbox className={`h-6 w-6 mr-3 ${homework.completions[0]?.isCompleted ? 'bg-green-600 border-0 text-white' : 'text-[#ededf2]'}`} checked={homework.completions[0]?.isCompleted}/>{homework.completions[0]?.isCompleted ? 'Задания выполнены' : 'Задания не выполнены'}
+                                                        <span
+                                                            className={`w-full mt-[16px] mb-2 h-[48px] text-black p-[16px] rounded-[16px] flex items-center cursor-pointer ${homework.completions[0]?.isCompleted ? 'bg-[#e8f7ea]' : 'bg-[#f4f4f8]'}`}>
+                                                            <Checkbox
+                                                                className={`h-6 w-6 mr-3 ${homework.completions[0]?.isCompleted ? 'bg-green-600 border-0 text-white' : 'text-[#ededf2]'}`}
+                                                                checked={homework.completions[0]?.isCompleted}/>{homework.completions[0]?.isCompleted ? 'Задания выполнены' : 'Задания не выполнены'}
                                                         </span>
                                                     </span>
                                                 ))}
@@ -127,11 +146,30 @@ export default function LessonSheet({ open, onOpenChange, item, date }: { open: 
                                                         <span className="flex flex-row items-center text-black">
                                                             {assignment.description}
                                                         </span>
-                                                        <span className={`w-full mt-[16px] h-[48px] p-[16px] rounded-[16px] flex items-center text-black cursor-pointer ${assignment.homeworkCompletion[0]?.isCompleted ? 'bg-[#e8f7ea]' : 'bg-[#f4f4f8]'}`}>
-                                                            <Checkbox className={`h-6 w-6 mr-3 ${assignment.homeworkCompletion[0]?.isCompleted ? 'bg-green-600 border-0 text-white' : 'text-[#ededf2]'}`} checked={assignment.homeworkCompletion[0]?.isCompleted}/>{assignment.homeworkCompletion[0]?.isCompleted ? 'Задания выполнены' : 'Задания не выполнены'}
+                                                        <span
+                                                            className={`w-full mt-[16px] h-[48px] p-[16px] rounded-[16px] flex items-center text-black cursor-pointer ${assignment.homeworkCompletion[0]?.isCompleted ? 'bg-[#e8f7ea]' : 'bg-[#f4f4f8]'}`}>
+                                                            <Checkbox
+                                                                className={`h-6 w-6 mr-3 ${assignment.homeworkCompletion[0]?.isCompleted ? 'bg-green-600 border-0 text-white' : 'text-[#ededf2]'}`}
+                                                                checked={assignment.homeworkCompletion[0]?.isCompleted}/>{assignment.homeworkCompletion[0]?.isCompleted ? 'Задания выполнены' : 'Задания не выполнены'}
                                                         </span>
                                                     </span>
                                                 ))}
+                                                <div className="space-y-2">
+                                                    {child.assessment && child.assessment.map((assessment, assessmentIndex) => (
+                                                        <div
+                                                            className="flex flex-row items-center justify-start">
+                                                            <div className={`flex items-center justify-center bg-[#f4f4f8] mr-2 rounded-md w-[43px] h-[43px] font-semibold ${setPropForItem(assessment.gradeType)}`} key={assessmentIndex}>
+                                                                <span>
+                                                                    {assessment.grade}
+                                                                    <sub className="text-[10px] align-baseline">{setIndexForGrade(assessment.gradeType)}</sub>
+                                                                </span>
+                                                            </div>
+                                                            <span>
+                                                                {assessment.gradeType}
+                                                            </span>
+                                                        </div>
+                                                    ))}
+                                                </div>
                                             </span>
                                         ))}
                                     </span>
